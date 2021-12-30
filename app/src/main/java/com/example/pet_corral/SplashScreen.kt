1189b29ed6ed1d27c050b37bb810708defcff47c
varun-02 <varuna.it.21@nitj.ac.in>
2021-12-31 00:30:58 +0530

    SplashScreen

---
+++ app/src/main/java/com/example/pet_corral/SplashScreen.kt
@@ -0,0 +1,33 @@
+package com.example.pet_corral
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+
+    private val splashScreen= 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val logoAnim: Animation = AnimationUtils.loadAnimation(this,R.anim.logo_anim)
+        val appLogo: ImageView = findViewById(R.id.imageView)
+
+        appLogo.animation = logoAnim
+
+        Handler().postDelayed({
+            startActivity(intent)
+            val intent = Intent(this@SplashScreen,LogIn::class.java)
+            startActivity(intent)
+            finish()
+        },splashScreen.toLong())
+    }
+}

e2d9f70605a561a216afcb8f647d963a6d45a74f
harshit <harshit.it.21@nitj.ac.in>
2021-12-30 23:21:32 +0530

    Pet-corral App

--- app/src/main/java/com/example/pet_corral/PetName.kt
+++ app/src/main/java/com/example/pet_corral/PetName.kt
@@ -3,11 +3,9 @@ package com.example.pet_corral
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PetName : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_pet_name)
     }
 }

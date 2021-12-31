a279e1622d0c6fc4ac5ffdff2085704601bcad9b
harshit <harshit.it.21@nitj.ac.in>
2021-12-31 23:59:29 +0530

    Pet-corral App

---
+++ app/src/main/java/com/example/pet_corral/buyOrSell.kt
@@ -0,0 +1,11 @@
+package com.example.pet_corral
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class buyOrSell : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy_or_sell)
+    }
+}

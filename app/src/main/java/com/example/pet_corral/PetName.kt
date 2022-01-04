65992e5ccc5702fa772d1b1c2b908c4151787b30
varun-02 <varuna.it.21@nitj.ac.in>
2022-01-04 21:41:14 +0530

    FireBase

--- app/src/main/java/com/example/pet_corral/PetName.kt
+++ app/src/main/java/com/example/pet_corral/PetName.kt
@@ -1,11 +1,23 @@
 package com.example.pet_corral
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PetName : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pet_name)
+        val button3 = findViewById<Button>(R.id.button3)
+        button3.setOnClickListener{
+            val intent = Intent(this,RecyclerView::class.java)
+            startActivity(intent)
+        }
+        val button4 = findViewById<Button>(R.id.button4)
+        button4.setOnClickListener {
+            val intent = Intent(this, RecyclerView::class.java)
+            startActivity(intent)
+        }
     }
 }
